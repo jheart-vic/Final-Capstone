1821294cfc85865642174003c1b1b37ddc4d2843
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-10-24 15:49:13 +0200

    add reservations controllers

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::ReservationsController < ApplicationController
+    def index 
+        @reservation = Reservation.all
+    end
+
+    def show
+        @reservation = Reservation.find(params[:id])
+    end
+
+    def create 
+        @reservation = Reservation.new(reservation_params)
+
+        if @reservation.save
+            render json: @reservation, status: :created, data: @reservation
+        else  
+            render json: @reservation.errors, status: :unprocessable_entity
+        end 
+    end
+
+    def update 
+        if @reservation.update(reservation_params)
+            render json: @reservation
+        else  
+            render json: @reservation.errors, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @reservation.destroy
+    end
+
+
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:date, :city)
+    end
 end
