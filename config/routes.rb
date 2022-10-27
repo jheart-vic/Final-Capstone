987cf33375646fc447d4af524766b3cdf0e6e1c9
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-10-27 16:51:57 +0200

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  # get '/users', to: 'users#index'
   get '/current_user', to: 'current_user#index'
-  # post '/api/v1/teachers', to: 'api/v1/teachers#index'
-  # post 'api/v1/reservations', to: 'api/v1/reservations#index'
 
   namespace :api do
     namespace :v1 do
